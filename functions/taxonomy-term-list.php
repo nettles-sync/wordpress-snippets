f0b8e386cf353adf7e9dff34b6d997befdf35547
nettles-sync <jnettsales@gmail.com>
2018-09-10 20:05:40 -0400

    update

--- functions/taxonomy-term-list.php
+++ functions/taxonomy-term-list.php
@@ -1,5 +1,6 @@
 <?php
 // comma separated list of taxonomy terms
+// output = Term 1, Term 2, Term 3
 function jn_resource_type_terms() {
 	$rt_terms = get_the_terms( get_the_ID() , 'YOUR_TAXONOMY_TERM' );
 	if ( ! empty( $rt_terms ) ) {
